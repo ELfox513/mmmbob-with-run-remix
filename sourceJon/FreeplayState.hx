a7a43d6d640540a5aab2b803a3dc6f9475468eed
Gliar <public.ms513@gmail.com>
2021-09-19 02:57:04 +0300

    Update FreeplayState.hx

--- sourceJon/FreeplayState.hx
+++ sourceJon/FreeplayState.hx
@@ -207,13 +207,13 @@ class FreeplayState extends MusicBeatState
 		
 		if (songs[curSelected].songName == 'run-remix-because-its-cool') { // ELabel
 			for (item in grpSongs.members) {
-				item.offset(new FlxPoint(random.int(0, 10), random.int(0, 10))) // Some shaking, I mean
+				item.offset(new FlxPoint(random.int(0, 10), random.int(0, 10))); // Some shaking, I mean
 			}
 			curDifficulty = 1;
 			diffText.text = "RUN";
 		} else {
 			for (item in grpSongs.members) {
-				item.offset(new FlxPoint())
+				item.offset(new FlxPoint());
 			}
 		}
 
